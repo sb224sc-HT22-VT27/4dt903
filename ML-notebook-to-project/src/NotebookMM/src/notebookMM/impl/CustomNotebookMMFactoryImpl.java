d284d75cb22beadf746806c3a804b8dfac0bfe44
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-11-03 14:27:00 +0100

    feat(NotebookMM): Added expected functionality of CustomXXXFactoryImpl.java

--- ML-notebook-to-project/src/NotebookMM/src/notebookMM/impl/CustomNotebookMMFactoryImpl.java
+++ ML-notebook-to-project/src/NotebookMM/src/notebookMM/impl/CustomNotebookMMFactoryImpl.java
@@ -1,17 +1,18 @@
 package notebookMM.impl;
 
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 
 import notebookMM.CodeCell;
-import notebookMM.CustomNotebookMMFactory;
 import notebookMM.MarkdownCell;
 import notebookMM.NotebookMMFactory;
 import notebookMM.NotebookMMPackage;
 import notebookMM.NotebookMetadata;
 import notebookMM.NotebookModel;
 
-public class CustomNotebookMMFactoryImpl extends NotebookMMFactoryImpl implements CustomNotebookMMFactory {
+public class CustomNotebookMMFactoryImpl extends NotebookMMFactoryImpl implements NotebookMMFactory {
 
 	public CustomNotebookMMFactoryImpl() {
 		super();
@@ -19,34 +20,60 @@ public class CustomNotebookMMFactoryImpl extends NotebookMMFactoryImpl implement
 
 	public static NotebookMMFactory init() {
 		try {
-			NotebookMMFactory theNotebookMMFactory = (NotebookMMFactory) EPackage.Registry.INSTANCE
+			NotebookMMFactory theNotebookFactory = (NotebookMMFactory) EPackage.Registry.INSTANCE
 					.getEFactory(NotebookMMPackage.eNS_URI);
-			if (theNotebookMMFactory != null)
-				return theNotebookMMFactory;
-		} catch (Exception e) {
-			EcorePlugin.INSTANCE.log(e);
+			if (theNotebookFactory != null) {
+				return theNotebookFactory;
+			}
+		} catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
 		}
-
-		return new CustomNotebookMMFactoryImpl();
+		return new NotebookMMFactoryImpl();
 	}
 
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+		case NotebookMMPackage.NOTEBOOK_MODEL:
+			return createNotebookModel(); // Will use custom implementation
+		case NotebookMMPackage.CODE_CELL:
+			return createCodeCell(); // Will use custom implementation
+		case NotebookMMPackage.MARKDOWN_CELL:
+			return createMarkdownCell(); // Will use custom implementation
+		case NotebookMMPackage.NOTEBOOK_METADATA:
+			return createNotebookMetadata(); // Will use custom implementation
+		default:
+			throw new IllegalArgumentException("Unknown class: " + eClass);
+		}
+	}
+
+	// Override create methods to return custom implementations
 	@Override
 	public NotebookModel createNotebookModel() {
-		return new CustomNotebookModelImpl();
-	}
-
-	@Override
-	public NotebookMetadata createNotebookMetadata() {
-		return new CustomNotebookMetadataImpl();
+		CustomNotebookModelImpl notebookModel = new CustomNotebookModelImpl();
+		return notebookModel;
 	}
 
 	@Override
 	public CodeCell createCodeCell() {
-		return new CustomCodeCellImpl();
+		CustomCodeCellImpl codeCell = new CustomCodeCellImpl();
+		return codeCell;
 	}
 
 	@Override
 	public MarkdownCell createMarkdownCell() {
-		return new CustomMarkdownCellImpl();
+		CustomMarkdownCellImpl markdownCell = new CustomMarkdownCellImpl();
+		return markdownCell;
+	}
+
+	@Override
+	public NotebookMetadata createNotebookMetadata() {
+		CustomNotebookMetadataImpl metadata = new CustomNotebookMetadataImpl();
+		return metadata;
+	}
+
+	@Override
+	public NotebookMMPackage getNotebookMMPackage() {
+		return (NotebookMMPackage) getEPackage();
 	}
 }
