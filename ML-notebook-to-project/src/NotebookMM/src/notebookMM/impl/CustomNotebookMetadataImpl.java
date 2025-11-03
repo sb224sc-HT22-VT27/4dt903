3f69c7d2369edb9932c51a8934d920abc2472bcf
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-11-03 14:28:51 +0100

    feat(NotebookMM): Added expected functionality of CustomXXXMetadataImpl.java

--- ML-notebook-to-project/src/NotebookMM/src/notebookMM/impl/CustomNotebookMetadataImpl.java
+++ ML-notebook-to-project/src/NotebookMM/src/notebookMM/impl/CustomNotebookMetadataImpl.java
@@ -1,5 +1,17 @@
 package notebookMM.impl;
 
 public class CustomNotebookMetadataImpl extends NotebookMetadataImpl {
+	@Override
+	public boolean isPythonKernel() {
+		String kernel = getKernelspec();
+		String lang = getLanguage();
 
+		if (kernel != null && kernel.toLowerCase().contains("python")) {
+			return true;
+		}
+		if (lang != null && lang.toLowerCase().contains("python")) {
+			return true;
+		}
+		return false;
+	}
 }
