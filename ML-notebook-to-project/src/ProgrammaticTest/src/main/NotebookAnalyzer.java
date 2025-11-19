3eddfebcff6d2cf4651637ae2f22c88b2361f3f0
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-11-19 14:10:53 +0100

    feat: Meeting w Diego fixed M2M basics

--- ML-notebook-to-project/src/ProgrammaticTest/src/main/NotebookAnalyzer.java
+++ ML-notebook-to-project/src/ProgrammaticTest/src/main/NotebookAnalyzer.java
@@ -1,23 +1,37 @@
 package main;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
+
 import notebookMM.CodeCell;
 import notebookMM.MarkdownCell;
 import notebookMM.NotebookModel;
 import notebookMM.util.NotebookJSONParser;
 
 public class NotebookAnalyzer {
+	private static ResourceSet rs;
 	private static final String inputDir = "../../input/";
+	private static final String[] fileName = { "Customer-Churn-Prediction/simple_classification",
+			"CNN-Image-Classifier/image_classification_cnn" };
+	private static final int inputFile = 1;
 
 	public static void main(String[] args) {
 		NotebookJSONParser parser = new NotebookJSONParser();
 
 		try {
 			// Parse .ipynb file
-			// NotebookModel notebook = parser.parseNotebook(inputDir +
-			// "Customer-Churn-Prediction/simple_classification.ipynb");
-
-			NotebookModel notebook = parser
-					.parseNotebook(inputDir + "CNN-Image-Classifier/image_classification_cnn.ipynb");
+			NotebookModel notebook = parser.parseNotebook(inputDir + fileName[inputFile] + ".ipynb");
 
 			// Validate
 			if (!notebook.validate()) {
@@ -88,8 +102,41 @@ public class NotebookAnalyzer {
 				}
 			}
 
+			setupResource();
+
+			save(notebook, "output/" + fileName[inputFile] + ".notebook");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void setupResource() {
+		rs = new ResourceSetImpl();
+
+		String[] fileextensions = new String[] { "notebook" };
+
+		for (String fileext : fileextensions) {
+			rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put(fileext, new XMLResourceFactoryImpl());
+		}
+
+	}
+
+	private static void save(NotebookModel notebook, String filename) {
+		URI uri = URI.createFileURI(filename);
+		Resource nbResource = rs.createResource(uri);
+		((ResourceImpl) nbResource).setIntrinsicIDToEObjectMap(new HashMap<String, EObject>());
+
+		nbResource.getContents().add(notebook);
+		System.out.println("In the middle of saving...");
+		Map<Object, Object> saveOptions = ((XMLResource) nbResource).getDefaultSaveOptions();
+		saveOptions.put(XMLResource.OPTION_CONFIGURATION_CACHE, Boolean.TRUE);
+		saveOptions.put(XMLResource.OPTION_USE_CACHED_LOOKUP_TABLE, new ArrayList<Object>());
+		try {
+			nbResource.save(saveOptions);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 }
