b7739acb6acd0864fa338c897311a282706f10ef
Samuel Fredric Berg <samuelberg.private@gmail.com>
2025-11-24 12:38:46 +0100

    feat: Fixes initail part of issue #30 TODO #32

---
+++ ML-notebook-to-project/src/NotebookToProjectM2M/src/transformationutils/CodeBlockClassifier.java
@@ -0,0 +1,28 @@
+package transformationutils;
+
+import java.util.Random;
+
+public class CodeBlockClassifier {
+	private static final String[] CLASSIFICATIONS = { "PREPROCESS", "TRAIN", "PREDICT" };
+	private static final Random random = new Random();
+
+	/**
+	 * Returns a random classification for a code block. This is a simple
+	 * implementation to verify the Java-QVTO call is working.
+	 * 
+	 * @return One of: PREPROCESS, TRAIN, or PREDICT
+	 */
+	public static String classifyCodeBlock() {
+		int index = random.nextInt(CLASSIFICATIONS.length);
+		return CLASSIFICATIONS[index];
+	}
+
+	/**
+	 * Returns a comment with the classification.
+	 * 
+	 * @return A comment string with the classification
+	 */
+	public static String getClassificationComment() {
+		return "\n# Classification: " + classifyCodeBlock() + "\n";
+	}
+}
